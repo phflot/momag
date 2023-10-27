24d9b5c37822d2eaddd3ff8aeecd221d7efa0cb0
Philipp <PhilippFlotho@gmail.com>
2023-10-27 19:27:59 +0200

    added an example script that demonstrates the usage of the neurovc util functions to run the magnification code on a video

---
+++ video_demo.py
@@ -0,0 +1,16 @@
+from neurovc.util import VideoReader, VideoWriter
+from neurovc.momag import OnlineLandmarkMagnifier
+import neurovc as nvc
+
+
+if __name__ == "__main__":
+    video_reader = VideoReader("vid.avi")
+    motion_magnifier = None
+    with VideoWriter("mag.avi",
+                     framerate=video_reader.framerate) as video_writer:
+        while video_reader.has_frames():
+            frame = video_reader.read_frame()[1]
+            if motion_magnifier is None:
+                motion_magnifier = OnlineLandmarkMagnifier(landmarks=nvc.LM_MOUTH + nvc.LM_EYE_LEFT + nvc.LM_EYE_RIGHT,
+                                                           reference=frame, alpha=15)
+            video_writer(motion_magnifier(frame))
